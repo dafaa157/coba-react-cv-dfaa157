3a4c63355c0be84015d56c1412125910b4ac12c2
dafaa157 <l200230157@student.ums.ac.id>
2025-11-28 01:18:47 +0700

    add fitur (exp)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Profile from "../components/Profile";
 import Skills from "../components/Skills";
+import Experiences from "../components/Experiences";
 
 function App() {
   return (
     <>
     <Profile />
     <Skills />
+    <Experiences />
+
     </>
   )
 }
